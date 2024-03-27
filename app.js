34b98b457c74ca9edf5e8a8df129f7be68f6694b
Antoine <antoine.beppo@gmail.com>
2024-03-27 17:53:30 +0100

    backend update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://grouptravel-one.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
